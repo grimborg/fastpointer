9b418a54e848f66d40c4374bf13e9a59f90ff356
Òscar Vilaplana <dev@oscarvilaplana.cat>
2013-10-05 12:46:28 +0200

    added parameters

--- fastpointer.go
+++ fastpointer.go
@@ -1,6 +1,9 @@
 package main
 
 import "io/ioutil";
+import "flag";
+import "fmt";
+import "strconv";
 
 func check(e error) {
     if e != nil {
@@ -9,10 +12,15 @@ func check(e error) {
 }
 
 func main() {
+    var speed = flag.Int("speed", 120, "Pointer speed");
+    var sensitivity = flag.Int("sensitivity", 250, "Pointer sensitivity");
+    flag.Parse()
+    fmt.Println("Speed:", *speed);
+    fmt.Println("Sensitivity:", *sensitivity);
     speedFile := "/sys/devices/platform/i8042/serio1/serio2/speed";
     sensitivityFile := "/sys/devices/platform/i8042/serio1/serio2/sensitivity";
-    err := ioutil.WriteFile(speedFile, []byte("120"), 0644);
+    err := ioutil.WriteFile(speedFile, []byte(strconv.Itoa(*speed)), 0644);
     check(err);
-    err = ioutil.WriteFile(sensitivityFile, []byte("250"), 0644);
+    err = ioutil.WriteFile(sensitivityFile, []byte(strconv.Itoa(*sensitivity)), 0644);
     check(err);
 }
